732c80f41fcadf166f08b6566b9489a76b1a3319
Elias Gomes <eliasgomesduraes2008@gmail.com>
2025-08-01 22:01:22 -0300

    Criei a variável decimal

---
+++ 02_Variaveis/DeclaracaoVariaveisDecimal.cs
@@ -0,0 +1,12 @@
+using System;
+
+class DeclaracaoVariaveisDecimal
+{
+	static void Main(string[] args)
+	{
+		//Decimal myMoneyError = 300.0;
+		decimal myMoneyCorrect = 300.0m;
+		Console.WriteLine(myMoneyCorrect);
+		
+	}
+}
