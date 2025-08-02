b26ed30c78991fa96a8bda3b7236fc88cc8ada4a
Elias Gomes <eliasgomesduraes2008@gmail.com>
2025-08-01 21:44:12 -0300

    Declaração de variáveis

---
+++ 02_Variaveis/DeclaracaoVariaveis.cs
@@ -0,0 +1,31 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+	
+static void Main()
+	{
+		/*
+		Dclaração de variavél
+		*/
+		
+		// Inicializando da Variável
+		int a;
+		a=22;
+		Console.WriteLine(a);
+		
+		//Reatribuição de valor na variável
+		a = 56;
+		Console.WriteLine(a);
+		
+		//Outras formas de declaração de variáveis
+		
+		//Declarando e inicializando a variável
+		int b = 925;
+		Console.WriteLine(b);
+		
+		int y = 10, x= 20;
+		int z = y + x;
+		Console.WriteLine(z);
+	}
+}
