d3a46fa9af4a5ee901fe578ba7c5189216883444
Elias Gomes <eliasgomesduraes2008@gmail.com>
2025-08-07 20:48:02 -0300

    Atualizei o programa e adicionei uma linha para identificar idoso

--- 03_DesvioCondicional/Idade.cs
+++ 03_DesvioCondicional/Idade.cs
@@ -4,20 +4,24 @@ class ClassificacaoIdade
 {
     static void Main()
     {
-		Console.WriteLine("Digite sua idade: ");
-		int idade = int.Parse(Console.ReadLine());
-		
-		if(idade <= 12) 
-		{
-			Console.WriteLine("Você é Criança");
-		}
-		else if (idade <= 17)
-		{
-			Console.WriteLine("Você é Adolescente");
-		}	
-		else
-		{
-			Console.WriteLine("Você é Adulto");
-		}
+        Console.WriteLine("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        
+        if (idade <= 12) 
+        {
+            Console.WriteLine("Você é Criança");
+        }
+        else if (idade <= 17)
+        {
+            Console.WriteLine("Você é Adolescente");
+        }    
+        else if (idade >= 60)
+        {
+            Console.WriteLine("Você é Idoso");
+        }
+        else // A única opção que sobrou é ser um adulto
+        {
+            Console.WriteLine("Você é Adulto");
+        }
     }
 }
