7e61f2f2ffb7f8044017b325d045c521cd2d8ef7
Elias Gomes <eliasgomesduraes2008@gmail.com>
2025-08-08 22:02:10 -0300

    Criei a tabuada usando loop FOR

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite o número:");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada do {numero}");
+            int resultado;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                 resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+        }
+    }
+}
