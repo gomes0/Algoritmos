ae797f505f6b8340fd9be53722818398f29595a3
Elias Gomes <eliasgomesduraes2008@gmail.com>
2025-08-22 21:19:27 -0300

    codígo do calendario

---
+++ 09_Calendario/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o mês (1.12): ");
+            int mes = int.Parse(Console.ReadLine() );
+
+            Console.Write("Digite o ano: ");
+            int ano = int.Parse(Console.ReadLine() );
+
+            //Descobre a quantidade de dias de um mês
+            int diasDoMes = DateTime.DaysInMonth(ano, mes);
+
+            //Descobre o dia da semana do primeiro dia do mês
+            // 0 = Domingo - 6 Sábado
+            //Gera o primeiro dia do mês e ano informado pelo usuário
+            DateTime primeiroDiaMes = new DateTime(ano, mes, 1);
+            int diaSemanaInicio = (int)primeiroDiaMes.DayOfWeek;
+
+            //Matriz de 6 semanas e 7 dias
+            int[,] calendario = new int[6,7];
+
+            int dia = 1;
+
+            //Preenche a matriz com os dias do mês
+            for (int semana = 0; semana < 6; semana++)
+            {
+                for (int diaSemana = 0; diaSemana < 7; diaSemana++) 
+                {
+                    if(diaSemana == 0 && diaSemana < diaSemanaInicio)
+                    {
+                        calendario[semana, diaSemana] = 0;
+                    }
+                    else if(dia <= diasDoMes)
+                    {
+                        calendario[semana, diaSemana] = dia;
+                        dia++;
+                    }
+                }
+            }
+        }
+    }
+}
